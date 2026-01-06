0b6b14e155cd52cae7c42f5a77dc3a71a755910c
Julio Arias <jarias@users.noreply.github.com>
2026-01-05 22:56:41 -0600

    revert changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,18 @@ pkg=$1
 
 sudo chown -R builder: /home/builder/src
 
+echo "----------------------> Building package $pkg"
 sudo chown -R alpm: /repo
 sudo pacman -Syu --noconfirm
 git clone https://aur.archlinux.org/${pkg}.git
 cd ${pkg}
-
-echo "----------------------> Building package $pkg"
 sed -i -E 's/^epoch=1$//gm;t' PKGBUILD
 makepkg -s --nocheck --noconfirm
-sudo mv *.pkg.tar.zst /repo
 echo "----------------------> Done building package $pkg"
 
-echo "----------------------> Updating repo $pkg"
-find . -type f -iname '*.pkg.tar.zst' -print0 | xargs -0 sudo repo-add -n /repo/jarias.db.tar.zst
-echo "----------------------> Done updating repo $pkg"
+echo "----------------------> Updating repo"
+sudo mv *.pkg.tar.zst /repo
+cd /repo
+find . -type f -iname '*.pkg.tar.zst' -print0 | xargs -0 sudo repo-add -n jarias.db.tar.zst
+sudo rm *.old || true
+echo "----------------------> Done updating repo"
